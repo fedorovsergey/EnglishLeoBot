75b32c2b71118f6a93dd5b99baa2602f13bea9ec
Sergey Fedorov <Fedorov.Sergey@tatar.ru>
2017-02-15 13:12:32 +0300

    Файлы заглушки для конфига

---
+++ app/config/database-example.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'host'     => 'localhost',
+    'user'     => 'user',
+    'password' => 'password',
+    'database' => 'database',
+];
---
+++ app/config/telegram-example.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'api_key'     => '123456789:abcdefghijklmnopqrstuvwxyzABCDEFGHI',
+    'bot_name'     => 'bot_name',
+];
